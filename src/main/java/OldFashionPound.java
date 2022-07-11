bf6f0f65e9a0cca6d742dbed3f45ac42b3fe133b
4NTC <antonio.collinassi@gmail.com>
2022-07-11 12:58:38 +0200

    Added Multiplication

--- src/main/java/OldFashionPound.java
+++ src/main/java/OldFashionPound.java
@@ -5,50 +5,75 @@ public class OldFashionPound {
 
   public static void main(String[] args) {
 
-    String input = "5p 17s 8d + 3p 4s 10d";
+    String input = "5p 17s 8d - 3p 4s 10d";
 
     String price = "(\\d+)p\\s(\\d+)s\\s(\\d+)d";
-    String sumOrDiv = "\\s*(\\d+p\\s\\d+s\\s\\d+d)\\s+([\\+-])\\s+(\\d+p\\s\\d+s\\s\\d+d)\\s*";
+    String sumOrSub = "\\s*(\\d+p\\s\\d+s\\s\\d+d)\\s+([\\+\\-])\\s+(\\d+p\\s\\d+s\\s\\d+d)\\s*";
+    String mulOrDiv = "\\s*(\\d+p\\s\\d+s\\s\\d+d)\\s+([\\*/]\\s+(\\d+)\\s*";
 
-    Pattern pattern = Pattern.compile(sumOrDiv);
+    Pattern pattern = Pattern.compile(sumOrSub);
     Matcher matcher = pattern.matcher(input);
+
+    OperationService operationService = new OperationService();
 
     if (matcher.matches()) {
       String firstPrice = matcher.group(1);
       String op = matcher.group(2);
       String secondPrice = matcher.group(3);
 
-      //SUM
+      Pattern digitPattern = Pattern.compile(price);
+      Matcher priceMatcher = digitPattern.matcher(firstPrice);
 
-      //Price 1
+      if (op.equals("+")) {
+
+        int totPenceFirstPrize = 0;
+        int totPenceSecondPrize = 0;
+
+        if (priceMatcher.matches()) {
+          totPenceFirstPrize = calculateTotPence(priceMatcher, operationService);
+        }
+
+        //System.out.println(totPenceFirstPrize);
+
+        priceMatcher = digitPattern.matcher(secondPrice);
+
+        if (priceMatcher.matches()) {
+          totPenceSecondPrize = calculateTotPence(priceMatcher, operationService);
+        }
+
+        //System.out.println(totPenceSecondPrize);
+
+        int totPencePrize = totPenceFirstPrize + totPenceSecondPrize;
+
+        //System.out.println(totPencePrize);
+
+        Pence pence = new Pence(totPencePrize);
+
+        Pound pound = operationService.penceToPound(pence);
+        int remainder = operationService.getRemainder();
+        pence = new Pence(remainder);
+        Shilling shilling = operationService.penceToShilling(pence);
+        remainder = operationService.getRemainder();
+        pence = new Pence(remainder);
+
+        System.out.printf("%sp %ss %sd\n", pound.getValue(), shilling.getValue(), pence.getValue());
+
+      }
+
+    }
+
+    pattern = Pattern.compile(mulOrDiv);
+
+    if (matcher.matches()) {
+
+      String firstPrice = matcher.group(1);
+      String op = matcher.group(2);
+      int num = Integer.parseInt(matcher.group(3));
 
       Pattern digitPattern = Pattern.compile(price);
       Matcher priceMatcher = digitPattern.matcher(firstPrice);
 
-      int totPenceFirstPrize = 0;
-      int totPenceSecondPrize = 0;
-
-      OperationService operationService = new OperationService();
-
-      if (priceMatcher.matches()) {
-        totPenceFirstPrize = calculateTotPence(priceMatcher, operationService);
-      }
-
-      System.out.println(totPenceFirstPrize);
-
-      //Price 2
-
-      priceMatcher = digitPattern.matcher(secondPrice);
-
-      if (priceMatcher.matches()) {
-        totPenceSecondPrize = calculateTotPence(priceMatcher, operationService);
-      }
-
-      System.out.println(totPenceSecondPrize);
-
-      int totPencePrize = totPenceFirstPrize + totPenceSecondPrize;
-
-      System.out.println(totPencePrize);
+      int totPencePrize = calculateTotPence(priceMatcher, operationService) * num;
 
       Pence pence = new Pence(totPencePrize);
 
@@ -57,7 +82,6 @@ public class OldFashionPound {
       pence = new Pence(remainder);
       Shilling shilling = operationService.penceToShilling(pence);
       remainder = operationService.getRemainder();
-      System.out.println(remainder);
       pence = new Pence(remainder);
 
       System.out.printf("%sp %ss %sd\n", pound.getValue(), shilling.getValue(), pence.getValue());
